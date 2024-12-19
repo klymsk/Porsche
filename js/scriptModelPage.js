eb836bce928da6aeba1ad117ae9456cbe6dd2cc7
SK <164775232+klymsk@users.noreply.github.com>
2024-12-19 23:12:02 +0200

    modelPageScript

---
+++ js/scriptModelPage.js
@@ -0,0 +1,52 @@
+const buttonModel = document.querySelector("#buttonModels");
+const subMenu = document.querySelector(".subMenu");
+
+const buttonContacts = document.querySelector("#buttonContacts");
+const contactsMenu = document.querySelector(".contactsMenu");
+
+const buttonServices = document.querySelector("#buttonServices");
+const servicesMenu = document.querySelector(".servicesMenu");
+
+const buttonAccessories = document.querySelector("#buttonAccessories");
+const accessoriesMenu = document.querySelector(".accessoriesMenu");
+
+const carChoiceItems = document.querySelectorAll('.carChoice li');
+
+function AddRemMenu(button, menu) {
+    button.addEventListener('mouseenter', () => {
+        menu.classList.add('visible');
+        menu.classList.remove('hidden');
+    });
+    button.addEventListener('mouseleave', () => {
+        menu.classList.add('hidden');
+        menu.classList.remove('visible');
+    });
+    
+    menu.addEventListener('mouseenter', () => {
+        menu.classList.add('visible');
+        menu.classList.remove('hidden');
+    });
+    
+    menu.addEventListener('mouseleave', () => {
+        menu.classList.add('hidden');
+        menu.classList.remove('visible');
+    });
+}
+
+AddRemMenu(buttonModel, subMenu);
+AddRemMenu(buttonContacts, contactsMenu);
+AddRemMenu(buttonServices, servicesMenu);
+AddRemMenu(buttonAccessories, accessoriesMenu);
+
+
+
+carChoiceItems.forEach(item => {
+    item.addEventListener('click', function() {
+        item.classList.toggle('active');
+    });
+});
+
+
+
+
+
